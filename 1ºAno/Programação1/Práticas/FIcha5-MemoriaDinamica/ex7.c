beb3ee8390e715c5083d68b0c80dd479739e907e
Diogo Alves <dgalves228@gmail.com>
2025-04-02 10:41:55 +0100

    added prog1 content

--- 1ºAno/Programação1/Práticas/FIcha5-MemoriaDinamica/ex7.c
+++ 1ºAno/Programação1/Práticas/FIcha5-MemoriaDinamica/ex7.c
@@ -2,4 +2,32 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct {
+    int num;
+    char *nome;
+    float preco;
 
+}Artigo;
+
+int main(){
+    Artigo a;
+    int contador=0;
+
+    printf("Diga o número do artigo: ");
+    scanf("%d",&a.num);
+    while(1){
+        if(a.nome=='\0')
+        break;
+
+        contador++;
+    }
+
+        a.nome = (char*)malloc(contador* sizeof(char));
+        if(a.nome==NULL){
+            printf("Ocorreu um erro ao alocar a memória!");
+            return 1;
+        }
+
+
+
+}
